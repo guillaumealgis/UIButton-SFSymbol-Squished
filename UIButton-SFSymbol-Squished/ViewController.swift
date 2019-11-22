2894174b00a93e7d9cf1df901befc1b24c5ca17f
Guillaume Algis <guillaume.algis@gmail.com>
2019-11-22 18:17:20 +0100

    Initial code commit

---
+++ UIButton-SFSymbol-Squished/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIButton-SFSymbol-Squished
+//
+//  Created by Guillaume Algis on 22/11/2019.
+//  Copyright © 2019 Guillaume Algis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
